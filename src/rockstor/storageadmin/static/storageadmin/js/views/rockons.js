22590bc0c0ad0c898e5ce0c45acf540f867ba357
FroggyFlox <flox2b@gmail.com>
2019-02-20 17:40:49 -0500

    Allow new networks to be specified directly from the field.

--- src/rockstor/storageadmin/static/storageadmin/js/views/rockons.js
+++ src/rockstor/storageadmin/static/storageadmin/js/views/rockons.js
@@ -1425,7 +1425,8 @@ RockonEditPorts = RockstorWizardPage.extend({
         this.$('.form-control').each(function(index, element) {
             $(this).select2({
                 dropdownParent: $('#install-rockon-overlay'),
-                width: '80%'
+                width: '80%',
+                tags: true
             });
         });
 
