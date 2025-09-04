f81c18c6401da6badac9d78b16af3ea72ac1874f
Philip Guyton <philip@yewtreeapps.com>
2025-09-04 19:34:02 +0100

    Upgrade to latest stable Poetry #2806
    - In build.sh use `--force` on pipx for poetry install/update.
    - Update poetry & poetry-plugin-dotenv versions to install.
    - Move pyproject.toml to the newer format expected by Poetry 2.*.
    - Add `requires-poetry` entry to [tool.poetry] section.
    - Add new section [tool.poetry.requires-plugins] to specify
    poetry-plugin-dotenv, with version.
    - Regenerate poetry.lock using poetry version 2.1.4.
    - Incidental minor Django update from 4.2.23 to 4.2.24.

--- build.sh
+++ build.sh
@@ -32,10 +32,10 @@ export PIPX_BIN_DIR=/usr/local/bin  # binary location for pipx-installed apps, d
 export PIPX_MAN_DIR=/usr/local/share/man  # manual page location for pipx-installed apps, default ~/.local/share/man
 # https://python-poetry.org/docs/#installing-with-pipx
 pipx ensurepath
-pipx install --python python3.11 poetry==1.7.1
+pipx install --force --python python3.11 poetry==2.1.4
 # https://pypi.org/project/poetry-plugin-dotenv/
 # https://python-poetry.org/docs/master/plugins/#using-plugins
-pipx inject --verbose poetry poetry-plugin-dotenv==0.6.11
+pipx inject --verbose poetry poetry-plugin-dotenv==3.0.1
 pipx list
 
 # Install project dependencies defined in cwd pyproject.toml using poetry.toml
