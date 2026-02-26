b97eb6f5908d70ee5754c2d2da27224ec7a039e1
Philip Guyton <philip@yewtreeapps.com>
2026-02-26 18:25:11 +0000

    Move Share chown chmod operations to the scheduler #3058
    Establish a Huey management task to serially execute each
    of chown and chmod, in their own tasks, when a Share access
    rights change is requested. Stamping the relevant Share with
    the management tasks unique task id. On management task
    completion, or failure, invoke via Huey signals a follow-up task
    to remove this stamp.
    
    Django admin-site additions re new Share.taskid field.
    
    On each ACL request first sanity check the associated Share.taskid.
    - Inform the user and reject the request if there is either a pending
      task, or an ongoing chown or chmod operation is identified via
      the psutil module. Otherwise ignore & overwrite the Share.taskid.
    
    Concurrency:
    - Extend concurrent scheduled workers from 2 to 4.
      We too easily reach exhaustion with only 2 workers.
      I.e. our regular ACL operation holds both workers:
      - 1. acl_change_manager
      -- 2. chown - awaited then:
      -- 2. chmod awaited.
    
    If another acl_change_manager is requested on another
    Share early in the above, we can have both workers held
    with just the two acl_change_mangers, each unable to
    instantiate their child tasks: hence moving to 4 workers.
    
    Incidental changes:
    - Mostly related string.format to fstring changes.
    - Use of system/constants.py for chown & chmod binary locations.
    - Huey Logs re redundancy and formating tweaks.

--- src/rockstor/storageadmin/views/share_acl.py
+++ src/rockstor/storageadmin/views/share_acl.py
@@ -18,13 +18,17 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 from os import stat, stat_result
 from stat import S_IMODE
 
+from huey.api import Task
+from huey.contrib.djhuey import db_task, HUEY
 from rest_framework.response import Response
 from django.db import transaction
 from storageadmin.models import Share
 from storageadmin.serializers import ShareSerializer
-from fs.btrfs import mount_share, umount_root, get_property
+from fs.btrfs import mount_share, get_property
+from storageadmin.util import handle_exception
 from storageadmin.views import ShareListView
-from system.acl import chown, chmod
+from storageadmin.views.scheduling_helpers import is_pending_task
+from system.acl import acl_change_manager, chown_or_chmod_active
 from system.users import user_name, group_name
 
 
@@ -33,9 +37,34 @@ class ShareACLView(ShareListView):
     def post(self, request, sid):
         with self._handle_exception(request):
             share = Share.objects.get(id=sid)
+            current_taskid = share.taskid
+            mnt_pt = share.mnt_pt
+            # Sanity check on current_taskid - ignore if no evidence of ongoing task.
+            if current_taskid is not None:
+                hi = HUEY
+                task_status = "unknown"
+                # Enqueue tasks are pending pre execution.
+                # There is a 1 to 3 second "pending" status for Huey tasks
+                if is_pending_task(hi, current_taskid):
+                    task_status = "pending"
+                # Executing tasks are no longer pending.
+                # Huey has limitations on feedback regarding executing tasks.
+                # I.e. task_status = hi.result(current_taskid, preserve = True)
+                # Returns None for both ongoing and non-existent task ids.
+                # See: https://github.com/coleifer/huey/issues/488
+                # CHECK FOR ONGOING RELATED ACTIVITY
+                if chown_or_chmod_active(mnt_pt=mnt_pt):
+                    task_status = "running"
+                if task_status != "unknown":
+                    e_msg = f"Change task, status ({task_status}), found for ({share.name}): task id ({current_taskid}) should complete in a few minutes."
+                    handle_exception(Exception(e_msg), request)
             # OWNER, GROUP, AND PERMISSIONS UPDATE.
             # Get the on disk subvol info.
-            mnt_pt = share.mnt_pt
+            was_unmounted: bool = False
+            if not share.is_mounted:
+                was_unmounted = True
+                # Filesystem rights access/changes require a mounted filesystem/subvol.
+                mount_share(share, mnt_pt)
             share_stat: stat_result = stat(mnt_pt)
             subvol_owner = user_name(share_stat.st_uid)
             subvol_group = group_name(share_stat.st_gid)
@@ -67,16 +96,37 @@ class ShareACLView(ShareListView):
             if share.compression_algo != compression:
                 share.compression_algo = compression
                 changed_fields.append("compression_algo")
+            # TASK INVOCATION
+            task_result_handle: Task = acl_change_manager(
+                mnt_pt,
+                owner=options["owner"],
+                group=options["group"],
+                og_recursive=options["orecursive"],
+                perms=options["perms"],
+                p_recursive=options["precursive"],
+                was_unmounted=was_unmounted,
+            )
+            # Store above task ID in Share; overwriting any "unknown" (orphaned) value.
+            share.taskid = task_result_handle.id
+            changed_fields.append("taskid")
             share.save(update_fields=changed_fields)
-
-            force_mount = False
-            if not share.is_mounted:
-                mount_share(share, mnt_pt)
-                force_mount = True
-            # TODO: Huey task that will return immediately, but are run asynchronously
-            #  around a second after being called.
-            chown(mnt_pt, options["owner"], options["group"], options["orecursive"])
-            chmod(mnt_pt, options["perms"], options["precursive"])
-            if force_mount is True:
-                umount_root(mnt_pt)
             return Response(ShareSerializer(share).data)
+
+
+@db_task()
+def clear_taskid(taskid: str | None = None):
+    """
+    Find Share with matching taskid to clear and update DB.
+    Called by @HUEY.signal(SIGNAL_COMPLETE) task_completed(signal, task) for relevant
+    tasks.
+    """
+    if taskid is None:
+        return None
+    try:
+        # Assumes there can be only one Share with this taskid.
+        share = Share.objects.get(taskid=taskid)
+    except Share.DoesNotExist:
+        return None
+    share.taskid = None
+    share.save(update_fields=["taskid"])
+    return None
