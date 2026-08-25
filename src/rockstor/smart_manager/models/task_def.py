6255e5df5cf5ebe43c2cc7a5e741d2ed0766e30e
Philip Guyton <philip@yewtreeapps.com>
2026-08-25 19:31:25 +0100

    Override TaskDefinition.delete to remove associated crontab line #3174

--- src/rockstor/smart_manager/models/task_def.py
+++ src/rockstor/smart_manager/models/task_def.py
@@ -1,5 +1,5 @@
 """
-Copyright (joint work) 2024 The Rockstor Project <https://rockstor.com>
+Copyright (joint work) 2026 The Rockstor Project <https://rockstor.com>
 
 Rockstor is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published
@@ -16,10 +16,10 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 import json
-
 from django.db import models
 from storageadmin.models import Pool, Share
 from smart_manager.constants import TASK_TYPES, TASK_SCRIPTS
+from system.task_util import remove_crontab
 
 
 class TaskDefinition(models.Model):
@@ -34,6 +34,30 @@ class TaskDefinition(models.Model):
 
     class Meta:
         app_label = "smart_manager"
+
+    def delete(self, **kwargs):
+        """
+        We override model.delete to ensure, if CRONTAB_FILE exists,
+        that matching entries are removed: but this will fail for mass opperations:
+        https://docs.djangoproject.com/en/6.0/topics/db/models/#overriding-predefined-model-methods
+        "Overridden model methods are not called on bulk operations"
+        Pre and Post delete signals are suggested as a work-around:
+        Model.post_delete is
+        "... sent at the end of a model’s delete() method and a queryset’s delete() method."
+        Ergo do not depend on this opperation for bulk delete opperations.
+        """
+        # Model instance.id is set to None by super().delete so stash first.
+        # Ref: https://docs.djangoproject.com/en/6.0/ref/models/instances/#django.db.models.Model.delete
+        script = self.script
+        instance_id = self.id
+        # Upstream call to do our SQL delete()
+        super_return = super().delete(**kwargs)
+        # "post_delete" signal equivalent.
+        try:
+            remove_crontab(script, instance_id)
+        except Exception:
+            pass
+        return super_return
 
     @property
     def share_name(self, *args, **kwargs):
